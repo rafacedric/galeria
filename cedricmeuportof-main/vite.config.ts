9462523e0ba96d58a2490755e1fbbccd65444fcb
rafacedric <rafelcedric@gmail.com>
2026-02-12 20:13:32 +0100

    so o basico final

--- cedricmeuportof-main/vite.config.ts
+++ cedricmeuportof-main/vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // Replace 'cedricmeuportof-main' with your actual repository name
+      base: '/cedriporataofuncionamain/', 
       server: {
         port: 3000,
         host: '0.0.0.0',
